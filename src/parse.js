fd4722246ee6b0e416f1dd0408775dc07dd538ab
Øystein Malt <me@oysmal.com>
2021-11-19 16:44:39 +0100

    Added a tokenizer and created a simple, initial parser using that

--- src/parse.js
+++ src/parse.js
@@ -3,6 +3,49 @@ import { l, lf, f } from "./core.js";
 const Node = "__$NODE__";
 const Closing = "__$CLOSING__";
 
+function tokenizer(source) {
+  const singleTokens = "()[]{}".split("");
+
+  const tokens = [];
+  let word = "";
+  let i = 0;
+
+  while (i < source.length) {
+    const char = source[i];
+
+    if (singleTokens.includes(char)) {
+      if (word.length > 0) {
+        tokens.push(word);
+        word = "";
+      }
+      tokens.push(char);
+    } else if (char === " " || char === "\n") {
+      if (word.length > 0) {
+        tokens.push(word);
+        word = "";
+      }
+    } else {
+      word += char;
+    }
+
+    ++i;
+  }
+  return tokens;
+}
+
+console.log(
+  tokenizer(`
+(lambda calc [a b]
+ (def res (add a b))
+ (add res b))
+(mult
+ 3
+ (calc 2 3)
+ (mult
+  (add 5))
+ 10)`)
+);
+
 export function parse(source) {
   const stack = [];
 
@@ -152,4 +195,101 @@ console.log(
   parseL(`
 (+ 1 2 3)
 `)()
+);
+
+function parseL2(source) {
+  const tokens = tokenizer(source);
+}
+
+function parseLF2(tokens, index) {
+  let i = index;
+  console.log("parse at index: ", i);
+  const token = tokens[i];
+  console.log("Current token: ", token);
+
+  if (token === ")") {
+    return { type: Closing, nextIndex: i + 1 };
+  }
+
+  if (token === "(") {
+    if (tokens[i + 1] === "(") {
+      const args = [];
+
+      let cur = parseLF2(tokens, i + 1);
+      while (cur?.type !== Closing) {
+        args.push(cur.value);
+        i = cur.nextIndex;
+        cur = parseLF2(tokens, i);
+      }
+
+      i = cur.nextIndex;
+      return { nextIndex: i, value: lf(args) };
+    } else {
+      const args = [];
+      const funcName = tokens[i + 1];
+      i += 1;
+
+      let cur = parseLF2(tokens, i + 1);
+      while (cur?.type !== Closing) {
+        args.push(cur.value);
+        i = cur.nextIndex;
+        cur = parseLF2(tokens, i);
+      }
+
+      if (cur.value !== undefined) {
+        args.push(cur.value);
+      }
+
+      i = cur.nextIndex;
+      return {
+        type: Closing,
+        nextIndex: i,
+        value: lf(lf(f, funcName), ...args),
+      };
+    }
+  } else if (token.charAt(0) === '"') {
+    return { nextIndex: i + 1, value: token };
+  } else if (token === "true" || token === "false") {
+    return { nextIndex: i + 1, value: token === "true" ? true : false };
+  } else if (token.match(/[0-9\-\+\.]+/)) {
+    return { nextIndex: i + 1, value: parseFloat(token) };
+  } else {
+    return { nextIndex: i + 1, value: token };
+  }
+}
+
+function logDFS(node) {
+  if (node?.type && node?.data) {
+    node.data.forEach(logDFS);
+  }
+  console.log(node);
+}
+
+logDFS(
+  parseLF2(
+    tokenizer(`
+(mult
+ 3
+ (mult
+  (add 5)
+  2)
+ 10)`),
+    0
+  ).value
+);
+
+console.log(
+  "Exection result: ",
+  l(
+    parseLF2(
+      tokenizer(`
+(mult
+ 3
+ (mult
+  (add 5)
+  2)
+ 10)`),
+      0
+    ).value
+  )
 );
