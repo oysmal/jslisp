f78aac5f09e06e7bb56144ae30bab9ecf49e825d
Øystein Malt <me@oysmal.com>
2021-12-09 11:39:48 +0100

    extract test code to test file

--- tests/index.js
+++ tests/index.js
@@ -1,67 +1,137 @@
-import {
-  l,
-  lf,
-  defn,
-  defg,
-  def,
-  add,
-  v,
-  f,
-  map,
-  lambda,
-  split,
-  str,
-  mult,
-  sub,
-} from "../src/core.js";
+import { l } from "../src/core.js";
+import { jslisp } from "../src/parse.js";
 
-// prettier-ignore
-const ftest = l(
-  lf(
-    defn,
-    "fntest",
-    ["a", "b"],
-      [
-          lf(def, "c", 7),
-          lf(add,
-             lf(v, "a"),
-             lf(v, "b"),
-             lf(v, "c"))]));
+function logDFS(node) {
+  if (node?.type && node?.data) {
+    node.data.forEach(logDFS);
+  }
+  console.log(node);
+}
 
-l(lf(defg, "c", 5));
+// logDFS(
+//   parseLF2(
+//     tokenizer(`
+// (mult
+//  3 //  (mult
+//   (add 5)
+//   2)
+//  10)`),
+//     0
+//   ).value
+// );
 
-// prettier-ignore
-console.log("TEST: ", l(lf(lf(f, "fntest"), 10,
-                           10)));
+// console.log(
+//   "Exection result: ",
+//   l(
+//     parseLF2(
+//       tokenizer(`
+// (mult
+//  3
+//  (mult
+//   (add 5)
+//   2)
+//  10)`),
+//       0
+//     ).value
+//   )
+// );
 
-// prettier-ignore
-const res = l(
-    lf(map,
-       lf(split,
-          lf(str, "1", ",", "2", ",", "3", ",", "4"),
-          ","),
-       lf(lambda,
-          ["x"],
-          [
-              lf(def, "a",
-                 lf(str,
-                    "Number: ",
-                    lf(v, "x"))),
-              lf(v, "a")])));
+// console.log("Without parsing: ", l(lf(def, "a", 2), lf(add, lf(v, "a"), 5)));
 
-console.log("Lambda: ", res);
+// console.log(
+//   "Exection result 2: ",
+//   l(parseLF2(tokenizer(`(mult 10 (add 5 2) 10 10)`), 0).value)
+// );
 
-console.log(l(lf(def, "test", 5), lf(v, "test")));
+// console.log("tokens: ", tokenizer(`((def a 2) (add (2 5))`));
+// console.log(
+//   "RESULT: ",
+//   l(parseLF2(tokenizer(`((defg a 2) (add (g a) 2))`), 0).value)
+// );
 
-const test = l(
-  lf(def, "myVar", 2),
-  lf(mult, lf(add, 1, lf(v, "myVar")), lf(sub, 5, 3))
-);
+// console.log(
+//   "RESULT: ",
+//   l(
+//     parseLF2(
+//       tokenizer(`
+// (
+//   (def a
+//     (cond
+//       (= 1 1)
+//       (* (+ 4 1 (- 2 1)) 3)
+//       (+ 1 1)))
+//   (+ (v a) 2)
+// )`),
+//       0
+//     ).value
+//   )
+// );
 
-console.log(test);
+// console.log(
+//   "RESULT: ",
+//   l(
+//     parseLF2(
+//       tokenizer(`((defn my/func [a b] ((pow (v a) (v b)))) (my/func 2 3))`),
+//       0
+//     ).value
+//   )
+// );
 
-/* THE GOAL:
-l(`
-(map [1 2 3 4] (lambda [x] (str "Number: " x)))
-`)
-*/
+// const programRes1 = jslisp(`(
+// (add 1 2)
+// )`);
+
+// console.log("ONE: ", programRes1);
+
+// const programRes = jslisp(`(
+// (defn my/test [a] (
+//   (str "Hello, " (v a))))
+
+// (my/test "Øystein Malt"))`);
+
+// console.log(programRes);
+
+// const myfn = jslisp(`(
+// (defn my/test [a] (
+//   (str "Hello, " (v a))))
+// (exportf my/test))`);
+
+// const myAdd = jslisp(`(
+// (defn my/add [a b] (
+//   (add (v a) (v b))))
+// (exportf my/add))`);
+
+const fib = jslisp(`(
+(defn my/fib [n] (
+  (cond (= (v n) 0)
+    (v n)
+    (cond (= (v n) 1)
+       (v n)
+       (+
+         (my/fib (- (v n) 1))
+         (my/fib (- (v n) 2)))))))
+(exportf my/fib))`);
+
+// console.log(myfn("Mememememe"));
+// console.log(myAdd(123123, 3434));
+
+const s1 = Date.now();
+const x = fib(25);
+const s2 = Date.now() - s1;
+console.log("jslisp time: ", s2, ", value: " + x);
+
+function fibbo(n) {
+  switch (n) {
+    case 0:
+    case 1:
+      return n;
+    default:
+      return fibbo(n - 1) + fibbo(n - 2);
+  }
+}
+
+const s3 = Date.now();
+const x1 = fibbo(25);
+const s4 = Date.now() - s3;
+
+console.log("pure js time: ", s4, ", value: " + x1);
